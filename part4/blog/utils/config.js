27f22a7feb970d7f422ca277002af35c1c9f1290
Kevin J.H Li <tensaikev@gmail.com>
2022-10-18 23:49:21 -0400

    config

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -5,6 +5,9 @@ const MONGODB_URL = process.env.MONGODB_URL;
 const SECRET = "randomstring";
 const random = "random";
 const random2 = "random";
+const random3 = "random";
+
+
 
 module.exports = {
   PORT,
