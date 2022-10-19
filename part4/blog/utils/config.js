b4a9ebb634c42314ff05820798f4b6912972b6a8
Kein L <tensaikev@gmail.com>
2022-10-19 03:52:32 +0000

    Update config.js

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -3,11 +3,6 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const MONGODB_URL = process.env.MONGODB_URL;
 const SECRET = "randomstring";
-const random = "random";
-const random2 = "random";
-const random3 = "random";
-
-
 
 module.exports = {
   PORT,
