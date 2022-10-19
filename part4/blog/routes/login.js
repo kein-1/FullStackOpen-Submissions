48b5dec97f12ab1f1bd4d403e6a2b72b82b16b54
Kein L <tensaikev@gmail.com>
2022-10-19 14:55:53 +0000

    Update login.js

--- part4/blog/routes/login.js
+++ part4/blog/routes/login.js
@@ -1,42 +1,41 @@
-require("dotenv").config()
-const express = require('express')
-const loginRouter = express.Router()
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
-const jsonwebtoken = require('jsonwebtoken')
+require("dotenv").config();
+const express = require("express");
+const loginRouter = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 
-const {SECRET} = require('../utils/config')
-
+const { SECRET } = require("../utils/config");
 
 //Check if the user is in the system. If so, we check if the password entered
-//is correct 
-loginRouter.post("/", async (request,response) => {
+//is correct
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
 
-    const {username,password} = request.body
+  //Find returns an array. Probably better to use findOne
+  const person = await User.find({ username: username });
 
-    //Find returns an array. Probably better to use findOne 
-    const person = await User.find({username:username})
+  if (person === undefined || person.length == 0)
+    return response.status(401).send("User not found!");
 
-    if (person === undefined || person.length == 0) return response.status(401).send("User not found!") 
+  //Compare the password that was passed with the hashed password using bcrypt since the actual passwords are not stored in the database
+  const passwordCorrect = await bcrypt.compare(
+    password,
+    person[0].passwordHash
+  );
 
-    //Compare the password that was passed with the hashed password using bcrypt since the actual passwords are not stored in the database
-    const passwordCorrect = await bcrypt.compare(password,person[0].passwordHash)
+  if (!passwordCorrect) return response.status(401).send("Wrong password!");
 
-    if (!passwordCorrect) return response.status(401).send("Wrong password!") 
+  const userForToken = {
+    username: person[0].username,
+    id: person[0]._id,
+  };
 
+  const token = jsonwebtoken.sign(userForToken, process.env.SECRET);
 
-    const userForToken = {
-        username: person[0].username,
-        id : person[0]._id
-    }
-    
-    const token = jsonwebtoken.sign(userForToken,process.env.SECRET)
+  response
+    .status(220)
+    .json({ token, username: person[0].username, name: person[0].name });
+});
 
-
-    response.status(220).json({ token, username: person[0].username, name: person[0].name })
-    
-
-})
-
-
-module.exports = loginRouter
+module.exports = loginRouter;
