bb657ba741f32256d40e67d161033aca0d9c30d0
Kein L <tensaikev@gmail.com>
2022-10-19 13:59:28 +0000

    Update config.js

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -4,11 +4,6 @@ const PORT = process.env.PORT || 3001;
 const MONGODB_URL = process.env.MONGODB_URL;
 const SECRET = "randomstring";
 
-const adding = "random str";
-const adding2 = "random str";
-const adding3 = "random str";
-const adding4 = "random str";
-
 module.exports = {
   PORT,
   MONGODB_URL,
