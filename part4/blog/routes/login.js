0b140efce95a7cde3e068e325f11751062838faa
Kein L <tensaikev@gmail.com>
2022-10-20 00:12:53 +0000

    Update login.js

--- part4/blog/routes/login.js
+++ part4/blog/routes/login.js
@@ -7,10 +7,6 @@ const jsonwebtoken = require("jsonwebtoken");
 
 const { SECRET } = require("../utils/config");
 
-//checking
-//checking
-//checking
-
 //Check if the user is in the system. If so, we check if the password entered
 //is correct
 loginRouter.post("/", async (request, response) => {
