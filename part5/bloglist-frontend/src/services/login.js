9684cf52d820d01da35cc81b5aa3d44bfb2542a1
Kevin J.H Li <tensaikev@gmail.com>
2022-11-14 12:16:59 -0500

    Updated files

--- part5/bloglist-frontend/src/services/login.js
+++ part5/bloglist-frontend/src/services/login.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-// const loginUrl = "http://localhost:3001/api/login";
+const loginUrl = "http://localhost:3001/api/login";
 // const loginUrl = "https://4rjbcc-3001.preview.csb.app/api/login";
 
 //THIS IS NOT THE RIGHT WAY OF DOING IT. I SHOULD SETUP A PRODUCTION AND DEVELOPMENT
 //ENVIRONMENTAL VARIABLES AND IT WILL RUN SEPARATE URLS BASED ON WHETHER OUR CODE IS
 //IN PRODUCTION OR DEVELOPMENT!!! LOOK INTO THIS !!
 //I WAS RUNNING INTO ERRORS WHEN I HOSTED BY SITE WITHOUT DOING THIS!!!
-const loginUrl = "https://blog-backend-9a3n.onrender.com/api/login";
+// const loginUrl = "https://blog-backend-9a3n.onrender.com/api/login";
 
 const login = async (credentials) => {
   try {
