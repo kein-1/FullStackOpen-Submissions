de3ebe14e142fda49e5ccc6aa120f83301c5eb9b
Kevin J.H Li <tensaikev@gmail.com>
2022-11-14 14:41:32 -0500

    updated .env

--- part5/bloglist-frontend/src/services/registration.js
+++ part5/bloglist-frontend/src/services/registration.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const registerUrl = "http://localhost:3001/api/registration";
+// const registerUrl = "http://localhost:3001/api/registration";
 // const registerUrl = "https://4rjbcc-3001.preview.csb.app/api/registration";
 
 //THIS IS NOT THE RIGHT WAY OF DOING IT. I SHOULD SETUP A PRODUCTION AND DEVELOPMENT
 //ENVIRONMENTAL VARIABLES AND IT WILL RUN SEPARATE URLS BASED ON WHETHER OUR CODE IS
 //IN PRODUCTION OR DEVELOPMENT!!! LOOK INTO THIS !!
 //I WAS RUNNING INTO ERRORS WHEN I HOSTED BY SITE WITHOUT DOING THIS!!!
-// const registerUrl = "https://blog-backend-9a3n.onrender.com/api/registration";
+const registerUrl = "https://blog-backend-walv.onrender.com/api/registration";
 
 const registration = async (userObject) => {
   const response = await axios.post(registerUrl, userObject);
